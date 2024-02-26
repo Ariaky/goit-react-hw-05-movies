ac1a692b705bd2191c448f13c80b800327f6d7c4
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-02-26 23:43:40 +0100

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,27 @@
 import { BallTriangle } from 'react-loader-spinner';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const Loader = () => {
   return (
-    <BallTriangle
-        height={100}
-        width={100}
-        radius={5}
-        color="#5c5470"
-        ariaLabel="ball-triangle-loading"
-        wrapperStyle={{display: 'flex', justifyContent: 'center'}}
-        wrapperClass=""
-        visible={true}
-        display={flex}
-        justifyContent={center}
+    <Wrapper>
+      <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#5c5470"
+          ariaLabel="ball-triangle-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          display={flex}
+          justifyContent={center}
     />
+    </Wrapper>
   );
 };
 
